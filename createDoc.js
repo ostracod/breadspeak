0388f030e03f2357b34acf34b44d07fcfc67ef60
John Eisenmann <esperantanaso@gmail.com>
2018-06-09 11:04:08 -0400

    Added style markup

--- createDoc.js
+++ createDoc.js
@@ -2,7 +2,23 @@
 var fs = require("fs");
 var escapeHtml = require("escape-html");
 
+var cssPath = "./documentation.css";
 var destinationPath = "./documentation.html";
+
+function generateStyleSpans(text) {
+    while (true) {
+        var index = text.indexOf("){");
+        if (index < 0) {
+            break;
+        }
+        var tempStartIndex = text.lastIndexOf("(", index - 1);
+        var tempEndIndex = text.indexOf("}", index + 2);
+        var tempClassName = text.substring(tempStartIndex + 1, index);
+        var tempContent = text.substring(index + 2, tempEndIndex);
+        text = text.substring(0, tempStartIndex) + "<span class=\"" + tempClassName + "\">" + tempContent + "</span>" + text.substring(tempEndIndex + 1, text.length);
+    }
+    return text;
+}
 
 var content = fs.readFileSync("./description.txt", "utf8");
 var lineList = content.split("\n");
@@ -20,7 +36,9 @@ while (true) {
         if (tempLine.charAt(0) == ">") {
             tempLine = "\u2022" + tempLine.substring(1, tempLine.length);
         }
-        paragraphLineList.push(escapeHtml(tempLine));
+        tempLine = escapeHtml(tempLine);
+        tempLine = generateStyleSpans(tempLine);
+        paragraphLineList.push(tempLine);
     } else {
         tempShouldAddParagraph = true;
     }
@@ -36,7 +54,9 @@ while (true) {
     }
 }
 
-var tempHead = "<head>\n<meta charset=\"UTF-8\">\n</head>";
+var cssContent = fs.readFileSync(cssPath, "utf8");
+
+var tempHead = "<head>\n<meta charset=\"UTF-8\">\n<style>\n" + cssContent + "\n</style>\n</head>";
 var tempBody = "<body>\n" + paragraphList.join("\n") + "\n</body>";
 var content = "<html>\n" + tempHead + "\n" + tempBody + "\n</html>";
 
